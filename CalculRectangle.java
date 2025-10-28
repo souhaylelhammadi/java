8ce2ba24c5b2f5ddc4ae016108228a62d29c0dc8
souhay <souhaylhmadi2005@gmail.com>
2025-10-28 20:54:52 +0100

    tp1

---
+++ CalculRectangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class CalculRectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Entrez la longueur du rectangle : ");
+        double longueur = input.nextDouble();
+        
+        System.out.print("Entrez la largeur du rectangle : ");
+       
+        double largeur = input.nextDouble();
+        double surface = longueur * largeur;
+        double perimetre = 2 * (longueur + largeur);
+
+        System.out.println("\n--- Résultats ---");
+        System.out.println("Longueur : " + longueur);
+        System.out.println("Largeur  : " + largeur);
+        System.out.println("Surface  : " + surface);
+        System.out.println("Périmètre: " + perimetre);
+
+        
+
+    }
+}
---
+++ CalculsSimples.java
@@ -0,0 +1,13 @@
+public class CalculsSimples {
+    public static void main(String[] args) {
+        int nombreEtudiants = 25;
+        double prixLivre = 149.99;
+        char lettrePreferee = 'J';
+        boolean coursTermine = false;
+
+        System.out.println("Nombre d'étudiants : " + nombreEtudiants);
+        System.out.println("Prix du livre : " + prixLivre + " MAD");
+        System.out.println("Lettre préférée : " + lettrePreferee);
+        System.out.println("Cours terminé ? " + coursTermine);
+    }
+}
